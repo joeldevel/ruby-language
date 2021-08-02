478cb8b2f1d8a3fcb92c4f236876207f8d39c99b
joeldevel <christianjoel.developer@gmail.com>
2021-08-02 19:06:59 -0300

    first steps basic ruby

---
+++ first_steps.rb
@@ -0,0 +1,17 @@
+# to run this: 
+#   $ ruby fist_steps.rb
+
+# this is a comment 
+
+# print something
+puts '*** to print something we use puts ***'
+puts 'Hoy es Lunes'
+puts "Today is Monday"
+
+a_list = ['floncho', 'urga', 'poti']
+
+# print list elements using each
+puts '*** print element from a list ***'
+a_list.each do | item |
+    puts "hello #{item}"
+end
