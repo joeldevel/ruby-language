8bdc1b3b7c8ddc896c3ff3d452ffc8c3cf385336
joeldevel <christianjoel.developer@gmail.com>
2021-08-04 21:57:32 -0300

    basic strings

---
+++ strings_1.rb
@@ -0,0 +1,15 @@
+# some strings in Ruby
+puts "*** Basic stuff ***"
+puts "This is a double-quoted string"
+puts "This is a single-quoted string"
+puts "This is a concatenated " + "string using '+'"
+puts 'We can print multiple times the string using \'*\'' * 3
+puts "We can print a number directly, such as "; puts 123
+
+puts "*** Cool stuf ***"
+puts "Using \`\` we can ask the OS to do thigs for us, such as 'ls'"
+puts "\`ls\` will list the items in the directory"
+puts `ls`
+# The next line creates a file called example.txt
+# puts `touch example.txt`
+
