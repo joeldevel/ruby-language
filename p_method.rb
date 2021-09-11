d0f4603b743fae0f4b8e3686c1592ebe1567885f
joeldevel <64231691+joeldevel@users.noreply.github.com>
2021-09-10 22:25:12 -0300

    Add p method example

---
+++ p_method.rb
@@ -0,0 +1,12 @@
+puts "banana"
+
+# the p method prints banana but surrounded by ""
+# thus telling us it is a string
+p "banana"
+
+puts "----"
+
+puts "\tba\tna\tna"
+
+# p prints the \t character
+p "\tba\tna\tna"
