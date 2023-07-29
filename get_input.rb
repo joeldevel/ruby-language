f1529943ff0cbcd60e5e3e09b5eb8b13c7ac784b
joeldevel <christianjoel.developer@gmail.com>
2023-07-29 12:20:19 -0300

    how to get user input

---
+++ get_input.rb
@@ -0,0 +1,15 @@
+puts "Welcome Human"
+puts "I'm Pipinus, a 🤖. Please tell me about you"
+
+puts "Quel est votre nom? "
+name = gets.chomp
+
+puts "Et Quel âge avez-vous? "
+age = gets.chomp.to_i 
+
+puts "So here is your data: "
+puts "\tname: #{name}"
+puts "\tage: #{age}"
+
+puts "That's all for now. Au revoir!"
+puts "🧉🧉🧉"
